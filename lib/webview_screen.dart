0f9e2135455aa4c14b84a52b86639da4c4b2f4ea
krumaska <okokrt@gmail.com>
2020-04-25 00:07:23 +0900

    Update webview_screen.dart include back chance

--- lib/webview_screen.dart
+++ lib/webview_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class WebViewScreen extends StatefulWidget {
   WebViewScreen({this.initurl});
@@ -9,18 +11,62 @@ class WebViewScreen extends StatefulWidget {
 }
 
 class _WebViewScreenState extends State<WebViewScreen> {
+  final flutterWebViewPlugin = FlutterWebviewPlugin();
+  bool isBackPressed = false;
+  bool canExit = false;
+  @override
+  void initState() {
+    super.initState();
+    flutterWebViewPlugin.onUrlChanged.listen((String url) {
+      print("current URL = $url");
+      setState(() {
+        isBackPressed = false;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    flutterWebViewPlugin.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      body: WebView(
-        initialUrl: widget.initurl,
-        javascriptMode: JavascriptMode.unrestricted,
-        // javascriptChannels: jsChannels,
-        onPageFinished: (String url) {
-          print('Page finished loading: $url');
+    return Scaffold(
+      body: WillPopScope(
+        onWillPop: () async {
+          // give chance to user to exit app
+          //if (canExit == true) SystemNavigator.pop();
+          if (isBackPressed == true) SystemNavigator.pop();
+          if (await flutterWebViewPlugin.canGoBack() == true) {
+            await flutterWebViewPlugin.goBack();
+          } else {
+            if (isBackPressed == false) {
+              await Fluttertoast.showToast(
+                msg: "앱을 종료하려면 한번 더 뒤로가기 해주세요.",
+                toastLength: Toast.LENGTH_SHORT,
+                gravity: ToastGravity.CENTER,
+                timeInSecForIosWeb: 1,
+                backgroundColor: Colors.lightBlue,
+                textColor: Colors.white,
+                fontSize: 16.0,
+              );
+            }
+            setState(() {
+              isBackPressed = true;
+            });
+          }
+          return false;
         },
+        child: SafeArea(
+          child: WebviewScaffold(
+            url: widget.initurl,
+            scrollBar: false,
+            hidden: true,
+          ),
+        ),
       ),
-    ));
+    );
   }
 }
